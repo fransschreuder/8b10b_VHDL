2f7309d921ca0cda60b088b295e37e2751f10b5a
Frans Schreuder <fransschreuder@gmail.com>
2020-11-12 17:02:10 +0100

    Added waveform viewer (gtkwave) to simulate-ghdl.sh script

--- scripts/simulate-ghdl.sh
+++ scripts/simulate-ghdl.sh
@@ -4,5 +4,6 @@ ghdl -a --std=08 -frelaxed-rules -P$(pwd)/../UVVM/ ../src/dec_8b10b.vhd
 ghdl -a --std=08 -frelaxed-rules -P$(pwd)/../UVVM/ ../tb/lookup_8b10b.vhd
 ghdl -a --std=08 -frelaxed-rules -P$(pwd)/../UVVM/ ../tb/validate_8b10b_tb.vhd
 ghdl -e --std=08 -frelaxed-rules -P$(pwd)/../UVVM/ validate_8b10b_tb
-ghdl -r --std=08 -frelaxed-rules -P$(pwd)/../UVVM/ validate_8b10b_tb
+ghdl -r --std=08 -frelaxed-rules -P$(pwd)/../UVVM/ validate_8b10b_tb --wave=validate_8b10b.ghw
+gtkwave validate_8b10b.ghw 
 
